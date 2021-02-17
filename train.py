2ddef102f6793b305a11dbbd0a37df77e70e29d7
FrankM <fjpavm@users.noreply.github.com>
2021-02-17 16:40:37 +0000

    First working training code.

--- train.py
+++ train.py
@@ -4,9 +4,11 @@ from tensorflow.keras import layers
 from matplotlib import pyplot as plt
 import os
 import ProfilingUtils
+import random
 
-syn_dataset_path = "../EdIntelligence-Neurolabs-Hackathon/common/train/syn/"
-real_dataset_path = "../EdIntelligence-Neurolabs-Hackathon/challenge-2/train/real/"
+dataset_path = dict()
+dataset_path['syn'] = "../EdIntelligence-Neurolabs-Hackathon/common/train/syn/"
+dataset_path['real'] = "../EdIntelligence-Neurolabs-Hackathon/challenge-2/train/real/"
 image_sub_path = "images/"
 challenge1_syn_csv = "annotations-detection.csv"
 challenge2_syn_csv = "annotations-localization.csv"
@@ -16,14 +18,17 @@ Image_Width = 1920
 Image_Height = 1080
 NumClasses = 330 + 1
 
+BATCH_SIZE = 1
+
 # Reads annotation file into a full annotations list, as well as by image and by class dicts
 # annotation is dict with keys:
 #   image: image name string
 #   class: class name string
 #   centre: np.array with x and y float pixel coordinates
 #   size: np.array with x and y float pixel dimentions
+#   source: where data comes from (default is syn)
 # returns annotations_list, annotations_by_image, annotations_by_class
-def read_annotation_file(path: str):
+def read_annotation_file(path: str, source = 'syn'):
     annotations_by_image = dict()
     annotations_by_class = dict()
     annotations_list = list()
@@ -36,7 +41,12 @@ def read_annotation_file(path: str):
                 xmax = float(xmax)
                 ymin = float(ymin)
                 ymax = float(ymax)
-                annotation = {"image":image_name, "centre" : np.array([(xmax+xmin)/2, (ymax+ymin)/2]), "size" : np.array([(xmax-xmin), (ymax-ymin)]), "class" : class_name}
+                annotation = {  "image":image_name, 
+                                "centre" : np.array([(xmax+xmin)/2, (ymax+ymin)/2]), 
+                                "size" : np.array([(xmax-xmin), (ymax-ymin)]), 
+                                "class" : class_name,
+                                "source" : source
+                            }
                 if image_name not in annotations_by_image:
                     annotations_by_image[image_name] = list()
                 annotations_by_image[image_name].append(annotation)
@@ -59,7 +69,7 @@ def createFigure(title=''):
 
     return fig
 
-# Adda a 5 pixel border in red channel
+# Add a 5 pixel border in red channel
 def highlightAnnotation(in_image, annotation):
     BBMin = annotation["centre"]-annotation["size"]/2
     BBMax = annotation["centre"]+annotation["size"]/2
@@ -90,6 +100,7 @@ def extractBestAnnotationForBlock(in_blockCentre, in_image_annotations):
 
 def createTrainingForImage(in_imageName, in_annotations_by_image, in_classMap):
     image_annotations = in_annotations_by_image[in_imageName]
+    #TODO: use input image dimentions to calculate prediction shapes
     BBPrediction = np.zeros((34,60,4))
     ObjectPrediction = np.zeros((34,60),dtype=int)
     ClassPrediction = np.zeros((34,60),dtype=int)
@@ -113,9 +124,52 @@ def createTrainingForImage(in_imageName, in_annotations_by_image, in_classMap):
                 ClassPrediction[blockY][blockX] = in_classMap['toInt'][bestAnnotation['class']]
     return BBPrediction, ObjectPrediction, ClassPrediction
 
+class DatasetFromImageNames_Generator(keras.utils.Sequence) :
+
+    def __init__(self, in_image_names, in_batch_size, in_annotations_by_image, in_classMap) :
+        self.m_image_names = in_image_names
+        self.m_batch_size = in_batch_size        
+        self.m_annotations_by_image = in_annotations_by_image
+        self.m_classMap = in_classMap
+
+    def __len__(self) :
+        return (np.ceil(len(self.m_image_names) / float(self.m_batch_size))).astype(np.int)
+    
+    def __getitem__(self, idx) :
+        batch_image_names = self.m_image_names[idx * self.m_batch_size : (idx+1) * self.m_batch_size]
+
+        batch_images = list()
+        batch_BBoxes = list()
+        batch_Objects = list()
+        batch_Classes = list()
+
+        #Create batch training data from filenames
+        for image_name in batch_image_names:
+            source = self.m_annotations_by_image[image_name][0]['source']
+            image_path = os.path.join(dataset_path[source], image_sub_path, image_name) 
+            image = plt.imread(image_path)
+            preprocessed = keras.applications.mobilenet_v2.preprocess_input(image)
+            batch_images.append(preprocessed)
+
+            BBPrediction, ObjectPrediction, ClassPrediction = createTrainingForImage(image_name, self.m_annotations_by_image, self.m_classMap)
+            batch_BBoxes.append(BBPrediction)
+            batch_Objects.append(ObjectPrediction)
+            batch_Classes.append(ClassPrediction)
+
+        return np.array(batch_images), [np.array(batch_BBoxes), np.array(batch_Objects), np.array(batch_Classes)]
+
+def separateData(list_of_data, numTrain=3200, numValidate=320, numTest=100):
+    list_of_data_copy = list(list_of_data)
+    random.shuffle(list_of_data_copy)
+    train_list = list_of_data_copy[0:numTrain]
+    validate_list = list_of_data_copy[numTrain:numTrain+numValidate]
+    test_list = list_of_data_copy[numTrain+numValidate:numTrain+numValidate+numTest]
+    return train_list, validate_list, test_list
+
+
 
 if __name__ == '__main__':
-    challenge1_csv_path = os.path.join(syn_dataset_path, challenge1_syn_csv) 
+    challenge1_csv_path = os.path.join(dataset_path['syn'], challenge1_syn_csv) 
     annotations_list, annotations_by_image, annotations_by_class = read_annotation_file(challenge1_csv_path)
     # create classMap for this run
     classMap = dict()
@@ -182,17 +236,34 @@ if __name__ == '__main__':
     intermediate_net_2 = keras.layers.DepthwiseConv2D((7,7), padding='same', activation='relu', name='class_net')(intermediate_net_1)
     class_out = keras.layers.Conv2D(NumClasses, (1,1), padding='same', activation='softmax', name='class_out')(intermediate_net_2)
 
-    mobileNetV2.summary()
-    model = keras.Model(input_layer, [boundingBox_out, object_out, class_out])
-    model.summary()
+    #mobileNetV2.summary()
+    
     BBmodel = keras.Model(input_layer, boundingBox_out)
     ObjModel = keras.Model(input_layer, object_out)
     ClassModel = keras.Model(input_layer, class_out)
 
+    model = keras.Model(input_layer, [boundingBox_out, object_out, class_out])
+    model.summary()
+    sparce_top5 = keras.metrics.SparseTopKCategoricalAccuracy(k=5)
+    
+    model.compile(optimizer='adam', 
+                    loss=['mse', 'sparse_categorical_crossentropy', 'sparse_categorical_crossentropy'], 
+                    loss_weights=[1.0, 1.0, 1.0],
+                    metrics={'BB_out':'mean_squared_error', 'obj_out':'sparse_categorical_accuracy', 'class_out':['sparse_categorical_accuracy', sparce_top5]})
 
-    img_count = 0
+    train_names, validate_names, test_names = separateData(annotations_by_image.keys())
+
+    train_gen = DatasetFromImageNames_Generator(train_names,BATCH_SIZE,annotations_by_image,classMap)
+    val_gen = DatasetFromImageNames_Generator(validate_names,BATCH_SIZE,annotations_by_image,classMap)
+    test_gen = DatasetFromImageNames_Generator(test_names,BATCH_SIZE,annotations_by_image,classMap)
+
+    model_checkpoint=keras.callbacks.ModelCheckpoint('../weights/EdIntel_weights{epoch:02d}.h5',save_weights_only=True)
+
+    model.fit(train_gen, batch_size=BATCH_SIZE, epochs=10, validation_data=val_gen, callbacks=[model_checkpoint])
+
+    """  img_count = 0
     images = dict()
-    image_path = os.path.join(syn_dataset_path, image_sub_path) 
+    image_path = os.path.join(dataset_path['syn'], image_sub_path) 
     for image_name in annotations_by_image.keys():
         image = plt.imread(os.path.join(image_path, image_name))
         img_count+=1
@@ -221,7 +292,7 @@ if __name__ == '__main__':
         #plt.close()
         fig = createFigure('image '+image_name)
         plt.imshow(image)
-        # TODO: draw boundery and label text
+        # TODO: draw label text
         plt.show(block=False)
         fig = createFigure('preprocessed '+image_name)
         plt.imshow(preprocessed)
@@ -230,4 +301,4 @@ if __name__ == '__main__':
             print(f"num images loaded: {img_count}")
             #print(f"image memory: {ProfilingUtils.getsize(images)}")
         
-    print(f"all images loaded: {img_count}")
+    print(f"all images loaded: {img_count}") """
